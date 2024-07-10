dfe9ad5fe1868fd2ba46bc0d6e865074c55801bf
Abdrakhman <abdrakhman@Abdrakhmans-MacBook-Pro.local>
2024-07-10 22:24:04 +0500

    auto save

--- init.lua
+++ init.lua
@@ -36,6 +36,9 @@ vim.opt.breakindent = true
 -- Save undo history
 vim.opt.undofile = true
 
+-- Turn off swap files
+vim.opt.swapfile = false
+
 -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
@@ -75,8 +78,19 @@ vim.opt.scrolloff = 10
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
 vim.opt.hlsearch = true
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
+-- Additional keymaps to make it easier to exit insert mode
 vim.keymap.set("i", "jj", "<Esc>")
 vim.keymap.set("i", "kk", "<Esc>")
+
+-- Additional vertical movements
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { noremap = true })
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { noremap = true })
+
+-- Center search on n/N
+vim.keymap.set("n", "n", "nzzzv", { noremap = true })
+vim.keymap.set("n", "N", "Nzzzv", { noremap = true })
+
 -- Diagnostic keymaps
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
@@ -173,7 +187,15 @@ require("lazy").setup({
 	},
 
 	{ "zbirenbaum/copilot.lua" },
-
+	{
+		"okuuva/auto-save.nvim",
+		cmd = "ASToggle", -- optional for lazy loading on command
+		event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
+		opts = {
+			-- your config goes here
+			-- or just leave it empty :)
+		},
+	},
 	{
 		"jiaoshijie/undotree",
 		dependencies = "nvim-lua/plenary.nvim",
@@ -907,6 +929,35 @@ require("copilot").setup({
 	},
 	copilot_node_command = "node", -- Node.js version must be > 18.x
 	server_opts_overrides = {},
+})
+
+-- Auto save files
+require("auto-save").setup({
+	enabled = true, -- start auto-save when the plugin is loaded (i.e. when your package manager loads it)
+	execution_message = {
+		enabled = true,
+		message = function() -- message to print on save
+			return ("AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S"))
+		end,
+		dim = 0.18, -- dim the color of `message`
+		cleaning_interval = 1250, -- (milliseconds) automatically clean MsgArea after displaying `message`. See :h MsgArea
+	},
+	trigger_events = { -- See :h events
+		immediate_save = { "BufLeave", "FocusLost" }, -- vim events that trigger an immediate save
+		defer_save = { "InsertLeave", "TextChanged" }, -- vim events that trigger a deferred save (saves after `debounce_delay`)
+		cancel_defered_save = { "InsertEnter" }, -- vim events that cancel a pending deferred save
+	},
+	-- function that takes the buffer handle and determines whether to save the current buffer or not
+	-- return true: if buffer is ok to be saved
+	-- return false: if it's not ok to be saved
+	-- if set to `nil` then no specific condition is applied
+	condition = nil,
+	write_all_buffers = false, -- write all buffers when the current one meets `condition`
+	noautocmd = false, -- do not execute autocmds when saving
+	lockmarks = false, -- lock marks when saving, see `:h lockmarks` for more details
+	debounce_delay = 1000, -- delay after which a pending save is executed
+	-- log debug messages to 'auto-save.log' file in neovim cache directory, set to `true` to enable
+	debug = false,
 })
 
 -- The line beneath this is called `modeline`. See `:help modeline`
